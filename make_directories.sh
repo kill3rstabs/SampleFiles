6e7f0c679041ff25abaa5ef7ea04a4f145e2aed4
kill3rstabs <nashitnoorali78@gmail.com>
2023-10-06 08:35:57 +0500

    edited make_directories.sh file

--- make_directories.sh
+++ make_directories.sh
@@ -1,47 +1,48 @@
-!mkdir DES_encryptedFiles
-%cd DES_encryptedFiles
-!mkdir Text
-!mkdir Zip
-!mkdir exe
-%cd ..
+mkdir DES_encryptedFiles
+cd DES_encryptedFiles
+mkdir Text
+mkdir Zip
+mkdir exe
+cd ..
 
-!mkdir AES_encryptedFiles
-%cd AES_encryptedFiles
-!mkdir Text
-!mkdir Zip
-!mkdir exe
-%cd ..
-
-
-!mkdir RSA_encryptedFiles
-%cd RSA_encryptedFiles
-!mkdir Text
-!mkdir Zip
-!mkdir exe
-%cd ..
+mkdir AES_encryptedFiles
+cd AES_encryptedFiles
+mkdir Text
+mkdir Zip
+mkdir exe
+cd ..
 
 
+mkdir RSA_encryptedFiles
+cd RSA_encryptedFiles
+mkdir Text
+mkdir Zip
+mkdir exe
+cd ..
 
 
 
-!mkdir DES_decryptedFiles
-%cd DES_decryptedFiles
-!mkdir Text
-!mkdir Zip
-!mkdir exe
-%cd ..
-
-!mkdir AES_decryptedFiles
-%cd AES_decryptedFiles
-!mkdir Text
-!mkdir Zip
-!mkdir exe
-%cd ..
 
 
-!mkdir RSA_decryptedFiles
-%cd RSA_decryptedFiles
-!mkdir Text
-!mkdir Zip
-!mkdir exe
-%cd ..
+
+mkdir DES_decryptedFiles
+cd DES_decryptedFiles
+mkdir Text
+mkdir Zip
+mkdir exe
+cd ..
+
+mkdir AES_decryptedFiles
+cd AES_decryptedFiles
+mkdir Text
+mkdir Zip
+mkdir exe
+cd ..
+
+
+mkdir RSA_decryptedFiles
+cd RSA_decryptedFiles
+mkdir Text
+mkdir Zip
+mkdir exe
+cd ..
